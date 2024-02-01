06f7e91cbbd2eba0155b0e994c3bd77f22de6ff5
sergi <sergio.ume@hotmail.com>
2024-02-01 12:22:35 +0000

    Primer commit

---
+++ src/main/java/es/pixar/comicom/ComicomApplication.java
@@ -0,0 +1,13 @@
+package es.pixar.comicom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComicomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComicomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/es/pixar/comicom/ComicomApplicationTests.java
@@ -0,0 +1,13 @@
+package es.pixar.comicom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComicomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
